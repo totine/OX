8b495a69a8471a60e7ed44782bbfeeb1b8805858
totine <joanna.gargas@gmail.com>
2018-04-25 18:51:47 +0200

    Improve questions

--- src/main/java/akademia/ox/StateQuestions.java
+++ src/main/java/akademia/ox/StateQuestions.java
@@ -5,17 +5,17 @@ public enum StateQuestions  {
     INITIAL_STATE {
         @Override
         public String get() {
-            return "Wpisz cokolwiek";
+            return "Wpisz cokolwiek, aby rozpocząć rundę (tu będą ustawienia rundy)";
         }
     }, GAME_IN_PROGRESS_STATE {
         @Override
         public String get() {
-            return "Wpisz victory, jeśli ma być zwycięstwo; Wpisz draw jeśli ma być remis, wpisz exit, aby zakończyć grę, wpisz cokolwiek, aby kontynuować rozgrywkę";
+            return "Wpisz victory, jeśli ma być zwycięstwo; Wpisz draw jeśli ma być remis, wpisz exit, aby zakończyć rundę 'walkowerem', wpisz cokolwiek, aby kontynuować rozgrywkę";
         }
     }, VICTORY_STATE {
         @Override
         public String get() {
-            return "Wpisz cokolwiek";
+            return "Wpisz cokolwiek, aby przejsć do podsumowania";
         }
     }, FINAL_STATE {
         @Override
@@ -30,7 +30,7 @@ public enum StateQuestions  {
     }, DRAW_STATE {
         @Override
         public String get() {
-            return "Wpisz cokolwiek";
+            return "Wpisz cokolwiek, aby przejść do podsumowania";
         }
     };
 
