36f10a126a7a590cd35a0c58a2157b1097827a7d
totine <joanna.gargas@gmail.com>
2018-04-26 17:32:30 +0200

    Add test for next move in InProgress State

--- src/test/java/akademia/ox/StateMachineWithPlayersTests.java
+++ src/test/java/akademia/ox/StateMachineWithPlayersTests.java
@@ -1,5 +1,6 @@
 package akademia.ox;
 
+import akademia.ox.states.InProgressState;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,22 +10,38 @@ public class StateMachineWithPlayersTests {
     Player playerOne = new Player("p1", "X");
     Player playerTwo = new Player("p2", "O");
 
-    private void setPlayers() {
+    private void setPlayers() throws IncorrectPlayerException, TooManyPlayersException {
         players = new Players();
         players.addNewPlayer(playerOne);
         players.addNewPlayer(playerTwo);
     }
 
     @Test
-    public void GameInProgress_onBeginningOnShowCurrentPlayer_returnPlayerOne() {
+    public void GameInProgress_onBeginningOnShowCurrentPlayer_returnPlayerOne() throws TooManyPlayersException, IncorrectPlayerException {
         //given
         setPlayers();
-        GameInProgress gameInProgress = new GameInProgress(players);
+        InProgressState gameInProgress = new InProgressState(players);
         //when
         Player currentPlayer = gameInProgress.showCurrentPlayer();
         //
         Assert.assertEquals(currentPlayer, playerOne);
     }
+
+    @Test
+    public void GameInProgress_inSecondTurnOnShowCurrentPlayer_returnPlayerTwo() throws TooManyPlayersException, IncorrectPlayerException {
+        //given
+        setPlayers();
+        InProgressState gameInProgress = new InProgressState(players);
+        //when
+        gameInProgress.consumeInput("here will be correct input");
+        gameInProgress.moveToNextState();
+
+        Player currentPlayer = gameInProgress.showCurrentPlayer();
+        //
+        Assert.assertEquals(currentPlayer, playerTwo);
+    }
+
+
 
 
 
