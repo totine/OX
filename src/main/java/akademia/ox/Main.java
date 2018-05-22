2a4b1c68b6355d710f7804e8d8c76853704ca665
totine <joanna.gargas@gmail.com>
2018-05-22 10:30:33 +0200

    Set numbers of players to 2

--- src/main/java/akademia/ox/Main.java
+++ src/main/java/akademia/ox/Main.java
@@ -30,7 +30,7 @@ public class Main {
         gameEnvProperties.setProperty("default-rows", "3");
         gameEnvProperties.setProperty("default-columns", "3");
         gameEnvProperties.setProperty("default-to-wins", "3");
-        gameEnvProperties.setProperty("numbers-of-player", "3");
+        gameEnvProperties.setProperty("numbers-of-player", "2");
         return gameEnvProperties;
 
     }
